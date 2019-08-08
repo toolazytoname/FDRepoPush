6ac411cac3def8cce3d96042739e60a8859e3084
weichao <weichao@yiche.com>
2019-08-08 15:37:39 +0800

    修改注释

--- FDLoseWeight.sh
+++ FDLoseWeight.sh
@@ -14,11 +14,11 @@
 #
 #
 #  如果要改原来的，在这里把分支保护给关掉http://gitlab.bitautotech.com/weichao/WelfareMirror/settings/repository  点击unprotect，记得完事后重新保护上
-# ./FDLoseWeight.sh  /Users/yiche/Code/test/WelfareMirror Example/Pods/ http://gitlab.bitautotech.com/weichao/WelfareThin
-# ./FDLoseWeight.sh  /Users/yiche/Code/test/WelfareMirror Example/ http://gitlab.bitautotech.com/weichao/WelfareThin
+#
+#
 #
 # 可以通过如下命令找到大文件
-#https://www.cnblogs.com/lout/p/6111739.html
+# https://www.cnblogs.com/lout/p/6111739.html
 # git verify-pack -v .git/objects/pack/pack-*.idx | sort -k 3 -g | tail -5
 # git rev-list --objects --all | grep 8f10eff91bb6aa2de1f5d096ee2e1687b0eab007
 #
@@ -53,30 +53,27 @@ echo "pwd resut:"
 pwd
 echo "开始瘦身:"
 
-#  .gitignore 自己手动加吧
+#  .gitignore 自己手动加吧,当然也可以用脚本加
 # echo "Example/Pods/" >> .gitignore
 # git add .gitignore
 # git commit -m "Add Example/Pods/ to .gitignore"
 
 git pull --all
 # 清除文件
-# git filter-branch --force --index-filter "git rm -r --ignore-unmatch --cached $2" --prune-empty --tag-name-filter cat -- --all
 oldIFS=$IFS
 IFS=,
 directory_to_remove_array=($2)
 IFS=$oldIFS
-for directory_name in ${directory_to_remove_array[@]}; do
-  git filter-branch --force --index-filter "git rm -r --ignore-unmatch --cached $directory_name" --prune-empty --tag-name-filter cat -- --all
-done
-
-# 如果文件很多，在下面补充
-# git filter-branch --force --index-filter 'git rm -r --cached --ignore-unmatch Example' --prune-empty --tag-name-filter cat -- --all
-
+# for directory_name in ${directory_to_remove_array[@]}; do
+  # git filter-branch --force --index-filter "git rm -r --ignore-unmatch --cached $directory_name" --prune-empty --tag-name-filter cat -- --all
+# done
 
 #Whenever you clone a repo, you do not clone all of its branches by default.
 #If you wish to do so, use the following script:
 for branch in `git branch -a | grep remotes/origin | grep -v HEAD | grep -v master `; do
    git branch --track ${branch#remotes/origin/} $branch
+   # echo "log: git branch --track ${branch#remotes/origin/} $branch"
+   # log: git branch --track RMLogin remotes/origin/RMLogin
 done
 
 # remotes/origin/RMLogin
@@ -86,7 +83,6 @@ done
 # for branch in  `git branch -r | grep -v 'HEAD\|master'`; do
 #  git branch --track ${branch##*/} $branch;
 # done
-# git branch --track experimental origin/experimental
 
 # 虽然上面我们已经删除了文件, 但是我们的repo里面仍然保留了这些objects, 等待垃圾回收(GC), 所以我们要用命令彻底清除它, 并收回空间.
 rm -rf .git/refs/original/
